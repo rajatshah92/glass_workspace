bb9f6265dd661c2e402fda13d60cd458c4060c73
rajatshah92 <rajatshah92@hotmail.com>
2014-06-20 16:24:59 -0400

    Adding a menu project

---
+++ AddingaMenu/src/com/glass/addingamenu/AddAMenu.java
@@ -0,0 +1,15 @@
+package com.glass.addingamenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddAMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_amenu);
+    }
+}
---
+++ VideoShow/src/com/glass/videoshow/VideoShow.java
@@ -0,0 +1,15 @@
+package com.glass.videoshow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class VideoShow extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_show);
+    }
+}
